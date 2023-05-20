1888368a54a06659c3766235e1bc48143cc671e2
Admin <sofiia.vynar.ir.2022@lpnu.ua>
2023-05-20 13:48:42 +0300

    Initial Commit

---
+++ Manager/AircraftManager.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class AircraftManager:
+    def __init__(self):
+        self.aircraft_list = []
+
+    def add_aircraft(self, aircraft):
+        self.aircraft_list.append(aircraft)
+
+    def find_all_with_max_speed_more_than(self, speed):
+        return list(filter(lambda a: a.max_speed > speed, self.aircraft_list))
+
+    def find_with_manufacturer(self, manufacturer):
+        return list(filter(lambda a: a.manufacturer == manufacturer, self.aircraft_list))
---
+++ Model/Drone.py
@@ -0,0 +1,19 @@
+from Model.Aircraft import Aircraft
+
+
+class Drone(Aircraft):
+    def __init__(self, manufacturer, max_speed, battery_capacity, energy_consumption_per_minute):
+        super().__init__(manufacturer, max_speed)
+        self.battery_capacity = battery_capacity
+        self.energy_consumption_per_minute = energy_consumption_per_minute
+
+    def get_max_flying_distance(self):
+        return int(self.battery_capacity / self.energy_consumption_per_minute * self.max_speed)
+
+    def get_max_delivery_weight(self):
+        return 0
+
+    def __str__(self):
+        return f"Drone(manufacturer={self.manufacturer}, max_speed={self.max_speed}, " \
+               f"battery_capacity={self.battery_capacity}, " \
+               f"energy_consumption_per_minute={self.energy_consumption_per_minute})"
